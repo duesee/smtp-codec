a327db72850a16dcbdc68dffd162ac853868a274
Damian Poddebniak <poddebniak@fh-muenster.de>
2021-05-15 19:26:20 +0200

    Add `number` parser.

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -4,6 +4,7 @@ use abnf_core::streaming::{is_ALPHA, is_DIGIT};
 use nom::{
     branch::alt,
     bytes::streaming::{tag, take_while},
+    character::streaming::digit1,
     combinator::{map_res, opt, recognize},
     sequence::tuple,
     IResult,
@@ -34,4 +35,8 @@ pub fn base64(input: &[u8]) -> IResult<&[u8], &str> {
     let (remaining, base64) = parser(input)?;
 
     Ok((remaining, base64))
+}
+
+pub fn number(input: &[u8]) -> IResult<&[u8], u32> {
+    map_res(map_res(digit1, from_utf8), str::parse::<u32>)(input) // FIXME(perf): use from_utf8_unchecked
 }
